4812c8b645f17c7f82cb60a455ee058b1816772c
Jinhwan Kim <hwanistic@gmail.com>
2023-10-27 09:18:00 +0900

    fix: contributor as not duplicate

--- main_page.py
+++ main_page.py
@@ -4,7 +4,6 @@ import requests
 import streamlit as st
 import pandas as pd
 
-
 headers = {'Authorization': 'token ' + st.secrets['TOKEN']}
 
 @st.cache(ttl = 3600*2) # if updated before 1 hour, not update again
@@ -67,15 +66,36 @@ def getStats(repo, org = 'statgarten', headers = headers):
     )
     return(df)
 
+def get_contributors_logins(owner, repo):
+    github_api_url = "https://api.github.com"
+    endpoint = f"{github_api_url}/repos/{owner}/{repo}/contributors"
+    
+    try:
+        response = requests.get(endpoint)
+        response.raise_for_status()  # HTTP 오류가 발생하면 예외를 발생시킵니다.
+        contributors = response.json()
+        logins = [contributor["login"] for contributor in contributors]  # 'login' 필드만 추출
+        return logins
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to retrieve contributors' logins: {e}")
+        return []
 
-def buildMetrics(metrics):
+def find_contributors(v, owner, repo):
+    data = get_contributors(owner, repo)
+    logins = [item['login'] for item in data]
+
+    v = v + logins
+    return(v)
+
+def buildMetrics(metrics, i):
     col1, col2, col3, col4, col5, col6 = st.columns(6)
 
     with col1:
         st.metric(label = '🛠️ 커밋', value = int(metrics['Commits']), delta = int(metrics[0])-1000) # 커밋 1000
 
     with col2:
-        st.metric(label = '🧑‍🤝‍🧑 기여자', value = int(metrics['Contributors']), delta = int(metrics[1])-30) # 기여자 30
+        # st.metric(label = '🧑‍🤝‍🧑 기여자', value = int(metrics['Contributors']), delta = int(metrics[1])-30) # 기여자 30
+        st.metric(label = '🧑‍🤝‍🧑 기여자', value = i, delta = i-30) # 기여자 30
 
     with col3:
         st.metric(label = '⭐ 스타', value = int(metrics['Stars']), delta = int(metrics[2])-200) # 스타 200
@@ -110,7 +130,7 @@ df = pd.concat([
     getStats('jstable', org = 'jinseob2kim'),
     getStats('jskm', org = 'jinseob2kim'),
     getStats('jsmodule', org = 'jinseob2kim'),
-    getStats('shiny.likert', org = 'zarathucorp'),        
+    getStats('shiny.likert', org = 'zarathucorp')      
     # kindergarten
     # statgarten.github.io
     # dispatch_test
@@ -121,7 +141,36 @@ df = df.set_index('Repo')
 
 Metrics = df.sum()
 st.markdown("### Statgarten: Total Metrics")
-buildMetrics(Metrics)
+
+# contributors 
+v = []
+v = find_contributors(v, 'statgarten', 'board')
+v = find_contributors(v, 'statgarten', 'colorpen')
+v = find_contributors(v, 'statgarten', 'datatoys')
+v = find_contributors(v, 'statgarten', 'datatoys-python')
+v = find_contributors(v, 'statgarten', 'datatoys-raw')
+v = find_contributors(v, 'statgarten', 'dockerImage')
+v = find_contributors(v, 'statgarten', 'door')
+v = find_contributors(v, 'statgarten', 'maps')
+v = find_contributors(v, 'statgarten', 'playdoh')
+v = find_contributors(v, 'statgarten', 'publicdata101')
+v = find_contributors(v, 'statgarten', 'scissor')
+v = find_contributors(v, 'statgarten', 'SGDS')
+v = find_contributors(v, 'statgarten', 'sgthemes')
+v = find_contributors(v, 'statgarten', 'soroban')
+v = find_contributors(v, 'statgarten', 'statgarten')
+v = find_contributors(v, 'statgarten', 'stove')
+v = find_contributors(v, 'jinseob2kim', 'jstable')
+v = find_contributors(v, 'jinseob2kim', 'jskm')
+v = find_contributors(v, 'jinseob2kim', 'jsmodule')
+v = find_contributors(v, 'zarathucorp', 'shiny.likert')
+
+# unique
+v = list(set(v))
+v = [item for item in v if item != 'github-actions[bot]']
+
+buildMetrics(Metrics, i = len(v))
 
 st.markdown("### Metrics for each repo")
 st.table(df)
+
